1383ab6db3b6bd4de8310ee3dd1c444863493551
madhephaestus <mad.hephaestus@gmail.com>
2016-06-01 14:33:27 -0400

    changing notation

--- moveRotate.groovy
+++ moveRotate.groovy
@@ -1,16 +1,17 @@
 //Move and rotate opperations
 double size =40;
-CSG cube = new Cube(	size,// X dimention
+CSG cube = new Cube(size,// X dimention
 			size,// Y dimention
 			size//  Z dimention
 			).toCSG()
-//cubeIntersectSphere = cubeIntersectSphere.move(1,2,3);// vector notation
+		
+//cubeIntersectSphere = cubeIntersectSphere.move(x,y,z);// vector notation
 CSG movedCube = cube
 			.movex(10)
 			.movey(20)
 			.movez(30)
 //rotate
-//cubeIntersectSphere = cubeIntersectSphere.rot(15,20,30);// vector notation
+//cubeIntersectSphere = cubeIntersectSphere.rot(rx,ry,rz);// vector notation
 movedCube = movedCube
 			.rotx(15)
 			.roty(20)
